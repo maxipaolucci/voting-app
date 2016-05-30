a8a9f2bc8f5726cbbf263bfabfbd00e2acbbe746
Maxi Office <maxipaolucci@gmail.com>
2016-05-31 10:34:41 +1200

    created the core with setEntries and next functions. Added tests

---
+++ voting-server/src/core.js
@@ -0,0 +1,16 @@
+/**
+ * Created by mpaoluc on 31/05/2016.
+ */
+import {List, Map} from 'immutable';
+
+export function setEntries(state, entries) {
+  return state.set('entries', List(entries));
+}
+
+export function next(state) {
+  const entries = state.get('entries');
+  return state.merge({
+    vote: Map({pair: entries.take(2)}),
+    entries: entries.skip(2)
+  });
+}

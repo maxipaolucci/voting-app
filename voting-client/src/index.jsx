c149ff199d6139cb6b864848c274dd08ce910074
Maxi Paolucci <maxipaolucci@gmail.com>
2016-07-12 19:30:27 +1200

    added routing and results component

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Voting from './components/Voting';
-
 require('./style.css');
 
-const pair = ['Trainspotting', '28 Days Later'];
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory} from 'react-router';
+import App from './components/App';
+import Voting from './components/Voting';
+import Results from './components/Results';
+
+const routes = <Route component={App}>
+  <Route path="/results" component={Results} />
+  <Route path="/" component={Voting} />
+</Route>;
 
 ReactDOM.render(
-  <Voting pair={pair} winner="Trainspotting"/>, document.getElementById('app')
+  <Router history={hashHistory}>{routes}</Router>,
+  document.getElementById('app')
 );

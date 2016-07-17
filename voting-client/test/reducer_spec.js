0e76f6fa634020194d915ac26fa89c1e632cb8ec
Maxi Paolucci <maxipaolucci@gmail.com>
2016-07-17 19:48:46 +1200

    removed currentUser on voting and vote components cause refarrals to index.jsx generates some kind of loop that fails when run tests

--- voting-client/test/reducer_spec.js
+++ voting-client/test/reducer_spec.js
@@ -67,10 +67,9 @@ describe('reducer', () => {
         }));
     });
 
-    it('handles VOTE by setting myVote', () => {
+    it('handles VOTE by returning the same state', () => {
         const state = fromJS({
             vote: {
-                round: 42,
                 pair: ['Trainspotting', '28 Days Later'],
                 tally: {Trainspotting: 1}
             }
@@ -80,64 +79,8 @@ describe('reducer', () => {
 
         expect(nextState).to.equal(fromJS({
             vote: {
-                round: 42,
                 pair: ['Trainspotting', '28 Days Later'],
                 tally: {Trainspotting: 1}
-            },
-            myVote: {
-                round: 42,
-                entry: 'Trainspotting'
-            }
-        }));
-    });
-
-    it('does not set myVote for VOTE on invalid entry', () => {
-        const state = fromJS({
-            vote: {
-                round: 42,
-                pair: ['Trainspotting', '28 Days Later'],
-                tally: {Trainspotting: 1}
-            }
-        });
-        const action = {type: 'VOTE', entry: 'Sunshine'};
-        const nextState = reducer(state, action);
-
-        expect(nextState).to.equal(fromJS({
-            vote: {
-                round: 42,
-                pair: ['Trainspotting', '28 Days Later'],
-                tally: {Trainspotting: 1}
-            }
-        }));
-    });
-
-    it('removes myVote on SET_STATE if round has changed', () => {
-        const initialState = fromJS({
-            vote: {
-                round: 42,
-                pair: ['Trainspotting', '28 Days Later'],
-                tally: {Trainspotting: 1}
-            },
-            myVote: {
-                round: 42,
-                entry: 'Trainspotting'
-            }
-        });
-        const action = {
-            type: 'SET_STATE',
-            state: {
-                vote: {
-                    round: 43,
-                    pair: ['Sunshine', 'Trainspotting']
-                }
-            }
-        };
-        const nextState = reducer(initialState, action);
-
-        expect(nextState).to.equal(fromJS({
-            vote: {
-                round: 43,
-                pair: ['Sunshine', 'Trainspotting']
             }
         }));
     });
